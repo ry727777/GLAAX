8cd7a68b8b5a02e1dbf6248c5d174285fc960ec4
ry727777 <ry727777@gmail.com>
2026-09-16 16:14:52 +0530

    Bitwise operator

---
+++ Bitwise.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(){
+	int a = 5;
+	int b = 6;
+	printf("%d\n",a&b); // & -> Bitwise and
+	printf("%d\n",a|b); // | -> Bitwise OR
+	
+	int c = 10;
+	int d = 12;
+	
+	printf("%d\n",c^d); // ^ -> Bitwise XOR
+	
+	int value = 5;
+	printf("Right shift 1 bit %d\n",100 >> 1);
+}
+
+
+
+
+
+
+
+
+
